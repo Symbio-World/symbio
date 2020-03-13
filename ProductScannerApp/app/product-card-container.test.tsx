8362a5c1cfb0b4e8b0e8e6811d592a6884f82b81
Almas Akchabayev <almeynman@gmail.com>
2020-03-13 15:45:48 +0200

    migrate to ts loose settings

--- ProductScannerApp/app/product-card-container.test.tsx
+++ ProductScannerApp/app/product-card-container.test.tsx
@@ -5,7 +5,6 @@ import { ActivityIndicator, Text } from 'react-native'
 
 import { ProductCardContainer } from './product-card-container';
 import { getProductData } from './get-product-data'
-import { testProduct } from './product.fixture'
 import { ProductCard } from './product-card'
 
 jest.mock('./get-product-data')
@@ -14,16 +13,16 @@ describe('ProductCardContainer', () => {
   it('renders correctly', () => {
     renderer.create(<ProductCardContainer barcode={'6414893012318'} />);
   })
-  
+
   it('renders loading screen at the start', () => {
-    testRenderer = renderer.create(<ProductCardContainer barcode={'6414893012318'} />);
+    const testRenderer = renderer.create(<ProductCardContainer barcode={'6414893012318'} />);
     const testInstance = testRenderer.root
     const activityIndicators = testInstance.findAllByType(ActivityIndicator)
     expect(activityIndicators.length).toBe(1)
   })
-  
+
   it('renders product card if data fetch succeeded', async () => {
-    const promise = Promise.resolve(testProduct)
+    const promise = Promise.resolve({})
     getProductData.mockImplementationOnce(() => promise)
     const testRenderer = renderer.create(<ProductCardContainer barcode={'6414893012318'} />);
     await promise
@@ -34,7 +33,7 @@ describe('ProductCardContainer', () => {
     const productCards = testInstance.findAllByType(ProductCard)
     expect(productCards.length).toBe(1)
   })
-  
+
   it('renders error card if error occurs', async () => {
     const promise = Promise.reject(new Error())
     getProductData.mockImplementationOnce(() => promise)
