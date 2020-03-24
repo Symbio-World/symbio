fb94ce4ab05395c2780a16b25a8680fcc4417f73
Almas Akchabayev <almeynman@gmail.com>
2020-03-24 14:09:43 +0200

    fix principles query

--- ProductScannerApp/app/principle/fetch-principles.ts
+++ ProductScannerApp/app/principle/fetch-principles.ts
@@ -6,7 +6,9 @@ export const fetchPrinciples: FetchPrinciples = async userId => {
   const snapshot = await firestore()
     .collection('events')
     .where('userId', '==', userId)
-    .orderBy('pricinples', 'asc')
+    .orderBy('timestamp', 'desc')
+    .orderBy('principles')
+    .limit(1)
     .get()
   return snapshot.docs.length > 0
     ? snapshot.docs[0].data().principles
