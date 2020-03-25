9f7a582f764b5e81593cbe6d27df98604f2ad88c
Almas Akchabayev <almeynman@gmail.com>
2020-03-25 19:25:26 +0200

    refactor

--- firebase-functions/index.ts
+++ firebase-functions/index.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions'
-import { parseProductPage } from '@symbio/conveyor-plugin-parser'
+import { parseProductPage } from '@symbio/conveyor-station-parser-cheerio'
 import { createConveyor } from '@symbio/conveyor-core'
 import {
   fetchSearchResponse,
