1d10f5fdc4e1fa4fa6b5aec6b059f89a3806ca7c
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-03-28 17:11:45 +0600

    feat(translate-object): accept different type of nested objects

--- fetcher-core/translate-object.test.ts
+++ fetcher-core/translate-object.test.ts
@@ -1,27 +1,191 @@
-import { createTranslateObject } from './translate-object'
+import { createTranslateObject, replaceValueWithInd, replaceIndWithValue } from './translate-object'
 
 describe('TranslateObject', () => {
+  // test helpers
+  it('replace array with string and obj where index instead of value where value only typeof string', async () => {
+    const obj = {
+      stringValue: 'firstValue'
+    }
+    const translatedObj = replaceValueWithInd(obj, [])
+    expect(translatedObj).toEqual({
+      array: ['firstValue'],
+      obj: { stringValue: 0 }
+    })
+  })
+
+  it('replace array with string and obj where index instead of value where value only typeof string and object', async () => {
+    const obj = {
+      stringValue: 'firstValue',
+      nestedObject: {
+        secondStringValue: 'secondValue',
+        secondsNestedObject: {
+          thirdStringValue: 'thirdValue',
+          thirdStringNestedObject: {
+            fourthStringValue: 'fourthStringValue',
+          }
+        }
+      }
+    }
+    const translatedObj = replaceValueWithInd(obj, [])
+    expect(translatedObj).toEqual({
+      array: ['firstValue', 'secondValue', 'thirdValue', 'fourthStringValue'],
+      obj: {
+        stringValue: 0,
+        nestedObject: {
+          secondStringValue: 1,
+          secondsNestedObject: {
+            thirdStringValue: 2,
+            thirdStringNestedObject: {
+              fourthStringValue: 3,
+            }
+          }
+        }
+      }
+    })
+  })
+  
+  it('replace index with value from array', async () => {
+    const obj = {
+      stringValue: 0,
+    }
+    const translatedObj = replaceIndWithValue(obj, ['firstValue'])
+    expect(translatedObj).toEqual({
+      stringValue: 'firstValue'
+    })
+  })
+  
+  it('replace index with value from array nested', async () => {
+    const obj = {
+      stringValue: 0,
+      nestedObject: {
+        secondStringValue: 1,
+        secondsNestedObject: {
+          thirdStringValue: 2,
+          thirdStringNestedObject: {
+            fourthStringValue: 3,
+          }
+        }
+      }
+    }
+    const translatedObj = replaceIndWithValue(obj, ['firstValue', 'secondValue', 'thirdValue', 'fourthStringValue'],)
+    expect(translatedObj).toEqual({
+      stringValue: 'firstValue',
+      nestedObject: {
+        secondStringValue: 'secondValue',
+        secondsNestedObject: {
+          thirdStringValue: 'thirdValue',
+          thirdStringNestedObject: {
+            fourthStringValue: 'fourthStringValue',
+          }
+        }
+      }
+    })
+  })
+  
+  it('replace value with index where value typeof array', async () => {
+    const obj = {
+      stringValue: ['firstValue', 'secondValue']
+    }
+    const translatedObj = replaceValueWithInd(obj, [])
+    expect(translatedObj).toEqual({
+      array: ['firstValue', 'secondValue'],
+      obj: { stringValue: [0,1] }
+    })
+  })
+  
+  it('replace array with string and obj where index instead of value where value only typeof string and object', async () => {
+  
+    const obj = {
+      stringValue: [
+        { firstValue: 'firstValue' },
+        { secondValue: 'secondValue' },
+        'thirdValue',
+        {
+          fourthStringValue: 'fourthStringValue',
+          nestedObject: {
+            fifthStringValue: 'fifthStringValue',
+          }
+        }
+      ]
+    }
+    const translatedObj = replaceValueWithInd(obj, [])
+    expect(translatedObj).toEqual({
+      array: ['firstValue', 'secondValue', 'thirdValue', 'fourthStringValue', 'fifthStringValue'],
+      obj: {
+        stringValue: [
+          { firstValue: 0 }, { secondValue: 1 }, 2, {
+            fourthStringValue: 3,
+            nestedObject: {
+              fifthStringValue: 4,
+            }
+          }
+        ]
+      }
+    })
+  })
+  it('replace index with value where value typeof array', async () => {
+    const obj = {
+      stringValue: [0,1]
+    }
+    const translatedObj = replaceIndWithValue(obj, ['firstValue', 'secondValue'])
+    expect(translatedObj).toEqual({
+      stringValue: ['firstValue', 'secondValue']
+    })
+  })
+
+  it('replace array with string and obj where index instead of value where value only typeof string and object', async () => {
+    const obj = {
+        stringValue: [
+          { firstValue: 0 }, { secondValue: 1 }, 2, {
+            fourthStringValue: 3,
+            nestedObject: {
+              fifthStringValue: 4,
+            }
+          }
+        ]
+      }
+
+    const translatedObj = replaceIndWithValue(obj, ['firstValue', 'secondValue', 'thirdValue', 'fourthStringValue', 'fifthStringValue'])
+    expect(translatedObj).toEqual({
+      stringValue: [
+        { firstValue: 'firstValue' },
+        { secondValue: 'secondValue' },
+        'thirdValue',
+        {
+          fourthStringValue: 'fourthStringValue',
+          nestedObject: {
+            fifthStringValue: 'fifthStringValue',
+          }
+        }
+      ]
+    })
+  })
+
   it('returns translated object', async () => {
     const translate = jest.fn(() =>
-      Promise.resolve(['ingredients', 's', 'q', 'f', 'c', 'origin']),
+      Promise.resolve(['ingredients', 'allergens1Title', 'allergens1Description', 'allergens2Title', 'allergens2Description', 'nutrients1Title', 'nutrients1Desctiption', 'nutrients2Title', 'nutrients2Desctiption', 'origin']),
     )
     const translatedObj = await createTranslateObject({ translate })(({
       ingredients: 'sealiha 55%,vesi',
       allergens: [
-        's', 'q'
+        { title: 'Ei sisällä', description: '	Maito ja maitotuotteet, myös laktoosi, Soijapavut ja soijapaputuotteet' },
+        { title: 'Ei sisällä', description: '	Maito ja maitotuotteet, myös laktoosi, Soijapavut ja soijapaputuotteet' },
       ],
       nutrients: [
-        'f', 'c'
+        { title: 'Energy', value: '984 kJ / 235 kcal' },
+        { title: 'Fat', value: 'asd',  },
       ],
       origin: 'Mundo',
     } as any))
     expect(translatedObj).toEqual({
       ingredients: 'ingredients',
       allergens: [
-        's', 'q'
+        { title: 'allergens1Title', description: 'allergens1Description' },
+        { title: 'allergens2Title', description: 'allergens2Description' },
       ],
       nutrients: [
-        'f', 'c'
+        { title: 'nutrients1Title', value: 'nutrients1Desctiption' },
+        { title: 'nutrients2Title', value: 'nutrients2Desctiption',  },
       ],
       origin: 'origin',
     })
--- fetcher-core/translate-object.ts
+++ fetcher-core/translate-object.ts
@@ -17,7 +17,7 @@ type NestedStringValueObject = {
   [key: string]: string | NestedStringValueObject
 }
 
-const checkValueType = (
+export const checkValueType = (
   {
     value = '',
     checkIsValueWrongType = (value: any) => {
@@ -29,80 +29,160 @@ const checkValueType = (
     onObject = (value: any) => value,
     onString = (value: any) => value,
     onNumber = (value: any) => value,
-  }: any) => {
+  }: any, ...args: any[]) => {
   checkIsValueWrongType(value)
   const isValueString = typeof value === 'string'
   const isValueNumber = typeof value === 'number'
   const isValueArray = Array.isArray(value)
 
   if (isValueNumber) {
-    return onNumber(value)
+    return onNumber(value, ...args)
   }
 
   if (isValueString) {
-    return onString(value)
+    return onString(value, ...args)
   }
   if (isValueArray) {
-    return onArray(value)
+    return onArray(value, ...args)
   }
 
-  onObject(value)
+  return onObject(value, ...args)
 }
 
-const replaceValueWithInd = (obj: any, array: any[]): any => {
+export const replaceValueWithInd = (obj: any, array: any[]): any => {
   const keys = Object.keys(obj)
   return keys.reduce((acc, k) => {
+    const handleValueTypeCheck= (value: any) => {
+      if (!value && !(typeof value === 'string')) {
+        throw new Error(`Encounter wrong value ${k}: ${obj[k]}`)
+      }
+    }
+    const handleStringValue = (value: any, parentAcc: any) => {
+      const newArray = [ ...parentAcc.array, value ]
+      const newObject = { ...parentAcc.obj, [k]: newArray.length - 1 }
+      return {
+        obj: newObject, array: newArray
+      }
+    }
+    const handleObjectValue = (value: any, parentAcc: any, currentIndex: number) => {
+      const newAcc = replaceValueWithInd(value, parentAcc.array)
+
+      if (Array.isArray(parentAcc.obj[k]) && !Number.isNaN(Number(currentIndex))) {
+        const arrayAcc = { ...parentAcc, array: newAcc.array }
+        arrayAcc.obj[k][currentIndex] = { ...parentAcc.obj[k][currentIndex], ...newAcc.obj }
+        return arrayAcc
+      }
+      return {
+        ...parentAcc, ...newAcc,
+        obj: {
+          ...parentAcc.obj,
+          [k]: {
+            ...parentAcc.obj[k],
+            ...newAcc.obj,
+          }
+        }
+      }
+    }
+    const handleArrayValue = (value: any[], parentAcc: any) => {
+      const newAcc = value.reduce((currentAcc, v, index: number) => {
+        if (typeof v === 'string') {
+          const newArray = [ ...currentAcc.array, v]
+          const newObjectArray = [ ...currentAcc.obj[k] ]
+          newObjectArray[index] = newArray.length - 1
+          return {
+            array: newArray,
+            obj: {
+              ...currentAcc.obj,
+              [k]: newObjectArray
+            }
+          }
+        }
+        const { array: newArray } = checkValueType({
+          value: v,
+          checkIsValueWrongType: handleValueTypeCheck,
+          onString: handleStringValue,
+          onArray: handleArrayValue,
+          onObject: handleObjectValue
+        }, currentAcc, index)
+        const newObjectArray = [ ...currentAcc.obj[k] ]
+
+        return {
+          array: newArray,
+          obj: {
+            ...currentAcc.obj,
+            [k]: newObjectArray
+          }
+        }
+      }, { obj: parentAcc.obj, array: parentAcc.array })
+      return newAcc
+    }
+
     return checkValueType({
       value: obj[k],
-      checkIsValueWrongType: (value: any) => {
-        if (!value && !(typeof value === 'string')) {
-          throw new Error(`Encounter wrong value ${k}: ${obj[k]}`)
-        }
-      },
-      onString: (value: any) => {
-        const newArray = [ ...acc.array, value ]
-        const newObject = { ...acc.obj, [k]: newArray.length - 1 }
-        return {
-          obj: newObject, array: newArray
-        }
-      },
-      onArray: (value: any[]) => {
-        const newArray = [ ...acc.array, ...value ]
-        const newObject = { ...acc.obj, [k]: [newArray.length - obj[k].length, newArray.length - 1] }
-        return {
-          obj: newObject, array: newArray
-        }
-      },
-      onObject: (value: any) => {
-        return { ...acc, ...replaceValueWithInd(value, acc.array) }
-      }
-    })
+      checkIsValueWrongType: handleValueTypeCheck,
+      onString: handleStringValue,
+      onArray: handleArrayValue,
+      onObject: handleObjectValue
+    }, { ...acc })
   }, { obj: { ...obj }, array: [ ...array ] })
 }
 
-const replaceIndWithValue = (obj: any, array: any[]): any => {
+export const replaceIndWithValue = (obj: any, array: any[]): any => {
   const keys = R.keys(obj)
   return keys.reduce((acc, k) => {
+    const handleValueTypeCheck= (value: any) => {
+      if (!value && !(typeof value === 'number')) {
+        throw new Error(`Encounter wrong value ${String(k)}: ${acc[k]}`)
+      }
+    }
+    const handleNumberValue = (value: any, parentAcc: any) => {
+      return { ...parentAcc, [k]: array[value] }
+    }
+    const handleObjectValue = (value: any, parentAcc: any, currentIndex: number) => {
+      const newAcc = replaceIndWithValue(value, array)
+      if (Array.isArray(parentAcc[k]) && !Number.isNaN(Number(currentIndex))) {
+        const arrayAcc = { ...parentAcc }
+        arrayAcc[k][currentIndex] = { ...parentAcc[k][currentIndex], ...newAcc }
+        return arrayAcc
+      }
+
+      return {
+        ...parentAcc,
+        ...(parentAcc[k] ? {
+          [k]: {
+            ...parentAcc[k],
+            ...newAcc,
+          }
+        }: newAcc)
+      }
+    }
+    const handleArrayValue = (value: any[], parentAcc: any) => {
+      return value.reduce((currentAcc, v, index: number) => {
+        if (typeof v === 'number') {
+          const newObjectArray = [ ...currentAcc[k] ]
+          newObjectArray[index] = array[currentAcc[k][index]]
+          return { ...currentAcc, [k]: newObjectArray }
+        }
+        const newObj = checkValueType({
+          value: v,
+          checkIsValueWrongType: handleValueTypeCheck,
+          onNumber: handleNumberValue,
+          onArray: handleArrayValue,
+          onObject: handleObjectValue
+        }, currentAcc[k][index])
+        const newObjectArray = [ ...currentAcc[k] ]
+        newObjectArray[index] = { ...currentAcc[k][index], ...newObj }
+        return { ...currentAcc, [k]: newObjectArray }
+      }, { ...parentAcc })
+    }
     return checkValueType({
       value: obj[k],
-      checkIsValueWrongType: (value: any) => {
-        if (!value && !(typeof value === 'number')) {
-          throw new Error(`Encounter wrong value ${String(k)}: ${obj[k]}`)
-        }
-      },
-      onNumber: (value: any) => {
-        return { ...acc, [k]: array[value] }
-      },
-      onArray: (value: any[]) => {
-        const [startInd, endInd] = value
-        const arrayOfValues = array.slice(startInd, endInd + 1)
-        return { ...acc, [k]: arrayOfValues }
-      },
-      onObject: (value: any) => {
-        return { ...acc, ...replaceIndWithValue(value, array) }
-      }
-    })
-  }, {})
+      checkIsValueWrongType: handleValueTypeCheck,
+      onNumber: handleNumberValue,
+      onArray: handleArrayValue,
+      onObject: handleObjectValue
+    }, acc)
+  }, { ...obj })
 }
 
 export const createTranslateObject: CreateTranslateObject = ({
