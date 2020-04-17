bdd862f3400a70f16cbc6d09f7abadc6f0ebcfb3
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-04-17 18:53:00 +0600

    feat: introduce SwipableModal

---
+++ react-native-app/app/ui-kit/SwipableModal.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react'
+import { Dimensions, View, StyleSheet } from 'react-native'
+import { t } from 'react-native-tailwindcss'
+import Animated from 'react-native-reanimated'
+
+import { Interactable } from './Interactable'
+import { ModalCard } from './ModalCard'
+
+const { Value, interpolate, concat, Extrapolate } = Animated
+const { width, height } = Dimensions.get('window')
+const deltaX = width / 2
+const α = Math.PI / 12
+const A = width * Math.cos(α) + height * Math.sin(α)
+
+type SwipableModalProps = {
+  onSnap: (e: { nativeEvent: { x: number } }) => void
+}
+
+export const SwipableModal: React.FC<SwipableModalProps> = ({
+  children,
+  onSnap,
+}) => {
+  const x = new Value(0)
+  const y = new Value(0)
+  const rotateZ = concat(
+    interpolate(x, {
+      inputRange: [-1 * deltaX, deltaX],
+      outputRange: [α, -1 * α],
+      extrapolate: Extrapolate.CLAMP,
+    }),
+    'rad',
+  )
+
+  const translateX = x
+  const translateY = y
+
+  const style = [
+    StyleSheet.absoluteFill,
+    {
+      transform: [{ translateX }, { translateY }, { rotateZ }],
+    },
+  ]
+
+  return (
+    <View style={[t.flex1]}>
+      <Interactable
+        style={style}
+        snapPoints={[{ x: -1 * A }, { x: 0 }, { x: A }]}
+        onSnap={onSnap}
+        x={x}
+        y={y}
+      >
+        <ModalCard>{children}</ModalCard>
+      </Interactable>
+    </View>
+  )
+}
