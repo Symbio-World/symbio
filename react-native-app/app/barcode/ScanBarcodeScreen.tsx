a3ad4883ca0c6bc3e3ba0af45eb555a8d9107436
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-04-17 22:28:01 +0600

    fix(ScanBarcodeScreen): sync with navigation

--- react-native-app/app/barcode/ScanBarcodeScreen.tsx
+++ react-native-app/app/barcode/ScanBarcodeScreen.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react'
 import { useIsFocused } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
-import { RootStackParamList } from '../Navigation'
+import { MainStackParamList } from '../Navigation'
 import { ScanBarcodeViewContainer } from './ScanBarcodeViewContainer'
 
 type Props = {
-  navigation: StackNavigationProp<RootStackParamList, 'ScanBarcodeScreen'>
+  navigation: StackNavigationProp<MainStackParamList, 'ScanBarcodeScreen'>
 }
 export const ScanBarcodeScreen: React.FC<Props> = ({ navigation }) => {
   const isFocused = useIsFocused()
 
   const handleScan = (barcode: string) => {
-    navigation.navigate('ProductScreen', { barcode })
+    navigation.navigate('Modals', {
+      params: { barcode },
+      screen: 'ProductScreen',
+    })
   }
 
   return <ScanBarcodeViewContainer onScan={handleScan} isActive={isFocused} />
