33df82fc6b4fb30c75b2581f93fe6d30ecd24d24
Almas Akchabayev <almeynman@gmail.com>
2020-04-19 04:34:12 +0300

    fix Info.plist path

--- react-native-app/scripts/update_ios_version_code.sh
+++ react-native-app/scripts/update_ios_version_code.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Updating version code to $1"
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $1" "./ios/Runner/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $1" "./ios/Symbio/Info.plist"
--- react-native-app/scripts/update_ios_version_name.sh
+++ react-native-app/scripts/update_ios_version_name.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Updating version name to $1"
-/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $1" "./ios/Runner/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $1" "./ios/Symbio/Info.plist"
