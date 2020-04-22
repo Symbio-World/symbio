836b0febe85231106b945b2a8b1e441306e036f3
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-04-22 10:29:04 +0600

    chore: enable screen before start

--- react-native-app/app/App.tsx
+++ react-native-app/app/App.tsx
@@ -4,9 +4,6 @@ import { StatusBar } from 'react-native'
 import RNBootSplash from 'react-native-bootsplash'
 import { AuthProvider } from './auth'
 import { Navigation } from './navigation/Navigation'
-import { enableScreens } from 'react-native-screens'
-
-enableScreens()
 
 export const App: React.FC = () => {
   React.useEffect(() => {
