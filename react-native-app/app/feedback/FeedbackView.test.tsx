c17cad6a55335473a281888ebefc31ff0d0e0df4
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-04-24 11:54:02 +0600

    feat(FeedbackView.test.tsx): check is keyboard dissmissed

--- react-native-app/app/feedback/FeedbackView.test.tsx
+++ react-native-app/app/feedback/FeedbackView.test.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react'
-import { TextInput } from 'react-native'
+import { TextInput, Keyboard } from 'react-native'
 import { Button } from '../ui-kit/Button'
 import { render, fireEvent } from 'react-native-testing-library'
+
 import { FeedbackView } from './FeedbackView'
+
+jest.mock('react-native/Libraries/Components/Keyboard/Keyboard', () => ({
+  addListener: jest.fn(),
+  dismiss: jest.fn(),
+}))
 
 describe('FeedbackView', () => {
   it('renders correctly', () => {
@@ -17,5 +23,14 @@ describe('FeedbackView', () => {
     fireEvent.changeText(getByType(TextInput), feedback)
     fireEvent.press(getByType(Button))
     expect(handleSubmit).toHaveBeenCalledWith(feedback)
+  })
+
+  it('triggers keyboard dissmiss', () => {
+    const feedback = 'feedback'
+    const handleSubmit = jest.fn()
+    const { getByType } = render(<FeedbackView onSubmit={handleSubmit} />)
+    fireEvent.changeText(getByType(TextInput), feedback)
+    fireEvent.press(getByType(Button))
+    expect(Keyboard.dismiss).toHaveBeenCalled()
   })
 })
