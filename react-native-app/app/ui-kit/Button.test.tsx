fdfc1e3fa61b9a9381b04feb1d4b27490e261de9
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-05-15 10:57:57 +0600

    feat(Button): introduce new styles

--- react-native-app/app/ui-kit/Button.test.tsx
+++ react-native-app/app/ui-kit/Button.test.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react'
 import { render } from 'react-native-testing-library'
-import { Button } from './Button'
+import { Button, ButtonStyle } from './Button'
 
 describe('Button', () => {
   it('renders correctly', () => {
     const { toJSON } = render(<Button />)
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('render transparent', () => {
+    const { toJSON } = render(
+      <Button buttonStyleType={ButtonStyle.TRANSPARENT} />,
+    )
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('render fill', () => {
+    const { toJSON } = render(<Button buttonStyleType={ButtonStyle.FILL} />)
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('render outline', () => {
+    const { toJSON } = render(<Button buttonStyleType={ButtonStyle.OUTLINE} />)
     expect(toJSON()).toMatchSnapshot()
   })
 })
--- react-native-app/app/ui-kit/Button.tsx
+++ react-native-app/app/ui-kit/Button.tsx
@@ -2,24 +2,58 @@ import * as React from 'react'
 import { TouchableOpacity, Text } from 'react-native'
 import { t } from 'react-native-tailwindcss'
 
+import { Icon, IconsEnum } from './Icon'
+
+export enum ButtonStyle {
+  FILL,
+  OUTLINE,
+  TRANSPARENT,
+}
+
 type Props = {
   onPress?: () => void
   title?: string
+  icon?: IconsEnum
+  buttonStyleType?: ButtonStyle
 }
-export const Button: React.FC<Props> = ({ onPress = () => {}, title = '' }) => {
+
+const ButtonBaseStyle = {
+  [ButtonStyle.FILL]: [t.bgPrimary],
+  [ButtonStyle.OUTLINE]: [t.bgWhite, t.border2, t.borderPrimary],
+  [ButtonStyle.TRANSPARENT]: [t.bgTransparent],
+}
+
+const InnerTitleBaseStyle = {
+  [ButtonStyle.FILL]: [t.textWhite],
+  [ButtonStyle.OUTLINE]: [t.textPrimary],
+  [ButtonStyle.TRANSPARENT]: [t.textPrimary],
+}
+
+export const Button: React.FC<Props> = ({
+  onPress = () => {},
+  title = '',
+  buttonStyleType = ButtonStyle.FILL,
+  icon = null,
+}) => {
+  const buttonStyle = ButtonBaseStyle[buttonStyleType]
+  const innerStyle = [InnerTitleBaseStyle[buttonStyleType]]
   return (
     <TouchableOpacity
       style={[
-        t.bgGreen500,
         t.p4,
-        t.alignCenter,
+        t.itemsCenter,
         t.justifyCenter,
         t.flex,
+        t.flexRow,
         t.rounded,
+        ...buttonStyle,
       ]}
       onPress={onPress}
     >
-      <Text style={[t.textCenter, t.text2xl, t.textWhite]}>{title}</Text>
+      {icon ? <Icon style={[t.mR2]} iconType={icon} /> : null}
+      <Text style={[t.textCenter, t.text2xl, t.textWhite, ...innerStyle]}>
+        {title}
+      </Text>
     </TouchableOpacity>
   )
 }
