4e167637665fc45957afafbde3f58187feb1b7a6
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-05-15 09:19:36 +0600

    fix: introduce svg type

---
+++ react-native-app/types/svg/index.d.ts
@@ -0,0 +1,7 @@
+declare module '*.svg' {
+  import { SvgProps } from 'react-native-svg'
+  const content: React.FC<SvgProps>
+  /* eslint-disable */
+  export default content
+  /* eslint-enable */
+}
