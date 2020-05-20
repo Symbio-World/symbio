f7a15aa8bd8a43d54b412553f0779f5d2b57ce7e
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-05-20 17:03:26 +0600

    chore: introduce more tests RoundedButton

--- react-native-app/app/ui-kit/RoundedButton.test.tsx
+++ react-native-app/app/ui-kit/RoundedButton.test.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { render } from 'react-native-testing-library'
+import { render, fireEvent } from 'react-native-testing-library'
 import { RoundedButton } from './RoundedButton'
 import { ButtonStyle } from './Button'
+import { Icons } from './Icon'
 
 describe('RoundedButton', () => {
   it('renders correctly', () => {
@@ -21,5 +22,25 @@ describe('RoundedButton', () => {
   it('render outline', () => {
     const { toJSON } = render(<RoundedButton variant={ButtonStyle.OUTLINE} />)
     expect(toJSON()).toMatchSnapshot()
+  })
+  it('render inner title', () => {
+    const { toJSON } = render(<RoundedButton innerTitle={'Button Test'} />)
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('render outer title', () => {
+    const { toJSON } = render(<RoundedButton outerTitle={'Button Test'} />)
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('render outer title and icon', () => {
+    const { toJSON } = render(
+      <RoundedButton outerTitle={'Close'} icon={Icons.CROSS} />,
+    )
+    expect(toJSON()).toMatchSnapshot()
+  })
+  it('simulate button click', () => {
+    const handlePress = jest.fn()
+    const { getByType } = render(<RoundedButton onPress={handlePress} />)
+    fireEvent(getByType(RoundedButton), 'onPress')
+    expect(handlePress).toBeCalled()
   })
 })
