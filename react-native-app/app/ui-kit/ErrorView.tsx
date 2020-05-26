b1da7c02cb69a5f1941db959a55202fc9103dd4e
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-05-26 13:21:53 +0600

    feat(ErrorView): design view

--- react-native-app/app/ui-kit/ErrorView.tsx
+++ react-native-app/app/ui-kit/ErrorView.tsx
@@ -1,14 +1,24 @@
 import * as React from 'react'
 import { View, Text } from 'react-native'
 import { t } from 'react-native-tailwindcss'
+import { Icon, Icons } from './Icon'
+import { CloseButtonView } from './CloseButtonView'
 
 type Props = {
   error?: unknown
+  onPress?: () => void
 }
-export const ErrorView: React.FC<Props> = () => {
+export const ErrorView: React.FC<Props> = ({ onPress = () => {} }) => {
   return (
-    <View style={[t.flex1, t.justifyCenter]}>
-      <Text>Something went wrong...</Text>
+    <View style={[t.flex1]}>
+      <Icon icon={Icons.FATAL_ERROR} />
+      <Text style={[t.textCenter, t.textBlack, t.text5xl, t.mB10]}>
+        OOOOPS!
+      </Text>
+      <Text style={[t.textCenter, t.textBlack, t.text3xl, t.mX12]}>
+        Something went wrong... You can tell us what happened or try again later
+      </Text>
+      <CloseButtonView onPress={onPress} />
     </View>
   )
 }
