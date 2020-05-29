436e5a92a98088d07b022f0756ab0be1280ad211
Almas Akchabayev <almeynman@gmail.com>
2020-05-29 15:26:29 +0300

    removed unused typeof

--- barcode-processor-core/failures.ts
+++ barcode-processor-core/failures.ts
@@ -2,14 +2,12 @@ import { createFailure } from '@symbio/ts-lib'
 import { ProductData } from './ProductData'
 
 export const NO_SEARCH_RESULTS_FOUND = 'NO_SEARCH_RESULTS_FOUND'
-export type NO_SEARCH_RESULTS_FOUND = typeof NO_SEARCH_RESULTS_FOUND
 export const noSearchResultsFound = (barcode: string) =>
   createFailure(NO_SEARCH_RESULTS_FOUND)(
     `Found no items for barcode ${barcode}`,
   )
 
 export const NO_USEFUL_INFO_FOUND = 'NO_USEFUL_INFO_FOUND'
-export type NO_USEFUL_INFO_FOUND = typeof NO_USEFUL_INFO_FOUND
 export const noUsefulInfoFound = (barcode: string, productData: ProductData) =>
   createFailure(NO_USEFUL_INFO_FOUND)(
     `No useful information found for barcode ${barcode}`,
