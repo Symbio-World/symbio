2ea5353c30919ff67944683e3f2b8691d91c0069
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-06-03 18:08:45 +0600

    fix(ErrorView): introduce dynamic font size

--- react-native-app/app/ui-kit/ErrorView.tsx
+++ react-native-app/app/ui-kit/ErrorView.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { View, Text } from 'react-native'
 import { t } from 'react-native-tailwindcss'
+import { RFPercentage } from 'react-native-responsive-fontsize'
+
 import { Icon, Icons } from './Icon'
 import { CloseButtonView } from './CloseButtonView'
 
@@ -8,14 +10,29 @@ type Props = {
   error?: unknown
   onClose?: () => void
 }
+
 export const ErrorView: React.FC<Props> = ({ onClose = () => {} }) => {
   return (
     <View style={[t.flex1]}>
       <Icon icon={Icons.FATAL_ERROR} />
-      <Text style={[t.textCenter, t.textBlack, t.text5xl, t.mB10]}>
+      <Text
+        style={[
+          t.textCenter,
+          t.textBlack,
+          t.mB10,
+          { fontSize: RFPercentage(7) },
+        ]}
+      >
         OOOOPS!
       </Text>
-      <Text style={[t.textCenter, t.textBlack, t.text3xl, t.mX12]}>
+      <Text
+        style={[
+          t.textCenter,
+          t.textBlack,
+          t.mX12,
+          { fontSize: RFPercentage(4) },
+        ]}
+      >
         Something went wrong... You can tell us what happened or try again later
       </Text>
       <CloseButtonView onClose={onClose} />
