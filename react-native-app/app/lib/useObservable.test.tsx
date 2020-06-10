49759b38d1ff652a33552d57821089918cf5c999
Almas Akchabayev <almeynman@gmail.com>
2020-06-10 18:12:49 +0300

    test unsubscribe

--- react-native-app/app/lib/useObservable.test.tsx
+++ react-native-app/app/lib/useObservable.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Text } from 'react-native'
-import { of, throwError } from 'rxjs'
-import { render } from 'react-native-testing-library'
+import { of, throwError, Observable } from 'rxjs'
+import { render, cleanup } from 'react-native-testing-library'
 import { useObservable } from './useObservable'
 
 describe('useObservable', () => {
@@ -23,5 +23,34 @@ describe('useObservable', () => {
     }
     const { getByText } = render(<Component />)
     expect(getByText(testError)).toBeDefined()
+  })
+
+  it('handles empty observable', () => {
+    const Component: React.FC<{}> = () => {
+      const { data } = useObservable<string>(of())
+      if (!data) return <Text>no data</Text>
+      return <Text>{data}</Text>
+    }
+    const { getByText } = render(<Component />)
+    expect(getByText('no data')).toBeDefined()
+  })
+
+  it('unsubscribes from observable', () => {
+    const subscription = {
+      unsubscribe: jest.fn(),
+    }
+    const observable = {
+      subscribe: () => subscription,
+    }
+    const Component: React.FC<{}> = () => {
+      const { data } = useObservable(
+        (observable as unknown) as Observable<string>,
+      )
+      return <Text>{data}</Text>
+    }
+
+    render(<Component />)
+    cleanup()
+    expect(subscription.unsubscribe).toHaveBeenCalledTimes(1)
   })
 })
