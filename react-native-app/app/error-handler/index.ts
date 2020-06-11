5f0253cfcdb1fb02a7ef4add98eaf5b500bb64d6
Adilkhan <adilkhankenzhetaev@gmail.com>
2020-06-11 12:20:49 +0600

    feat(error-handler): log error

--- react-native-app/app/error-handler/index.ts
+++ react-native-app/app/error-handler/index.ts
@@ -2,13 +2,16 @@ import RNRestart from 'react-native-restart'
 import { setJSExceptionHandler } from 'react-native-exception-handler'
 import { Alert } from 'react-native'
 
+import { saveError } from './saveError'
+
 setJSExceptionHandler((error: Error, isFatal: boolean) => {
   if (isFatal) {
+    saveError(error.message)
     Alert.alert(
       'Unexpected error occurred',
       `
-          Error: ${isFatal ? 'Fatal:' : ''} ${error.name} ${error.message}
-
+          Sorry, ${isFatal ? 'fatal:' : ''} error happen by name ${error.name}.
+          Our team already looking into issue.
           We will need to restart the app.
           `,
       [
---
+++ react-native-app/app/error-handler/saveError.ts
@@ -0,0 +1,64 @@
+import { frontendError } from '@symbio/event-store-core'
+import DeviceInfo from 'react-native-device-info'
+import { storeEvent } from '../event'
+import { Platform } from 'react-native'
+
+type SaveError = (message: string) => Promise<void>
+
+const getGeneralInfo = async () => {
+  const [brand, bundleId, deviceType, deviceName] = await Promise.all([
+    Promise.resolve(DeviceInfo.getBrand()), //general
+    Promise.resolve(DeviceInfo.getBundleId()), //general
+    Promise.resolve(DeviceInfo.getDeviceType()), //general
+    DeviceInfo.getDeviceName(), //general
+  ])
+  return { brand, bundleId, deviceType, deviceName }
+}
+
+const getIOSLogs = async () => {
+  const [generalInfo] = await Promise.all([getGeneralInfo()])
+  const info = {
+    ...generalInfo,
+  }
+  return JSON.stringify(info)
+}
+
+const getAndroidLogs = async () => {
+  const [
+    generalInfo,
+    apiLevel,
+    baseOs,
+    device,
+    firstTimeInstallTime,
+    lastUpdateTime,
+  ] = await Promise.all([
+    getGeneralInfo(),
+    DeviceInfo.getApiLevel(),
+    DeviceInfo.getBaseOs(),
+    DeviceInfo.getDevice(),
+    DeviceInfo.getFirstInstallTime(),
+    DeviceInfo.getLastUpdateTime(),
+  ])
+  const info = {
+    ...generalInfo,
+    apiLevel,
+    baseOs,
+    device,
+    firstTimeInstallTime,
+    lastUpdateTime,
+  }
+  return JSON.stringify(info)
+}
+
+export const getDeviceInfo = Platform.select({
+  ios: getIOSLogs,
+  android: getAndroidLogs,
+  default: getIOSLogs,
+})
+
+export const saveError: SaveError = (message) =>
+  getDeviceInfo()
+    .then((info: string) => storeEvent(frontendError(message, info)))
+    .catch((e: Error) =>
+      console.warn('Error handler handle error =)', e.message),
+    )
