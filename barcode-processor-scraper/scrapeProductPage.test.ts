6a620768ec16a469547428ca4e32e55b740b6dd5
andywindowsmac <adilkhankenzhetaev@gmail.com>
2020-06-30 21:58:48 +0600

    fix(scrapeProductPage): introduce selver badges

--- barcode-processor-scraper/scrapeProductPage.test.ts
+++ barcode-processor-scraper/scrapeProductPage.test.ts
@@ -30,12 +30,10 @@ describe('scrapeProductPage', () => {
   it('scrapes selver', () => {
     expect(scrapeSelver(selver.html)).toEqual({
       ingredients:
-        'PIIM, kirsikaste 19% (kirss, suhkur, vesi, paksendaja modifitseeritud tärklis, happesuse regulaator sidrunhape, säilitusaine kaaliumsorbaat), suhkur, PETT, tärklis, osaliselt hüdrogeenitud palmiõli, vähendatud rasvasisaldusega kakaopulber, RÕÕSK KOOR, PETIpulber, emulgaator (E433, E435, E471, E472b, E475), stabilisaator (E407, E410), sool, toiduvärv E160a, lõhna- ja maitseaine.',
-      allergens: [
-        'Piim',
-        'Võib sisaldada vähesel määral erinevaid pähkleid. Võib sisaldada kirsikive.',
-      ],
+        'Vesi, porgand, kartul, suhkrumais, kanafilee 9%, sibul, rapsiõli, till. Aedviljade osakaal 61%.',
+      allergens: ['', ''],
       origin: 'Eesti',
+      tags: ['Gluten free', 'Lactose free'],
     })
   })
 
--- barcode-processor-scraper/scrapeProductPage.ts
+++ barcode-processor-scraper/scrapeProductPage.ts
@@ -61,6 +61,38 @@ export const scrapeBarbora = (html: string) => {
   }
 }
 
+const SELVER_TAGS_HREF = {
+  /* eslint-disable  @typescript-eslint/naming-convention */
+
+  // TODO: discuss which badges we need
+  // 'parim_lihatoode_2019.jpg': 'The best meat product',
+  'laktoosivaba_40x40.jpg': 'Lactose free',
+  'gluteenivaba_40x40.jpg': 'Gluten free',
+  'suhkruvaba_40x40.jp': 'Sugar free',
+  'mahetoode_40x40.jpg': 'Organic product',
+  // 'okotoode_40x40.jpg': '', // TODO: Ask Almas meaning
+  'vegan_40x40.jpg': 'Vegan',
+  // 'tunnustatud_eesti_maitse.jpg': 'Recognized Estonian taste',
+  // 't_istera-m_rgis.jpg': '', // TODO: Ask Almas meaning
+  // 'e-poe_margid_saaremaa.jpg': '', // TODO: Ask Almas meaning
+  // 'eesti_parim_toiduaine_40x40.jpg': 'The best food in estonia',
+
+  /* eslint-enable  @typescript-eslint/naming-convention */
+}
+
+const scrapeSelverTags = ($: CheerioStatic) => {
+  return $('ul.product-badges')
+    .find('img')
+    .toArray()
+    .map((t) => {
+      const linkSplitted = t.attribs.src.split('/')
+      const badgeLink = linkSplitted[linkSplitted.length - 1]
+      const badge = (SELVER_TAGS_HREF as { [key: string]: string })[badgeLink]
+      return badge
+    })
+    .filter((v) => v)
+}
+
 export const scrapeSelver = (html: string) => {
   const $ = cheerio.load(html)
   return {
@@ -70,6 +102,7 @@ export const scrapeSelver = (html: string) => {
       $('h5:contains("Hoiatused")').next().text().trim(),
     ],
     origin: $('th:contains("Päritolumaa")').next().text().trim(),
+    tags: scrapeSelverTags($),
   }
 }
 
